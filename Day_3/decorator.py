d9bb8b66a267e7f9a137da14d6c764af881fffbc
Kishore <kishorepunniyamoorthi@gmail.com>
2026-07-20 07:35:52 +0530

    @property decorators

--- Day_3/decorator.py
+++ Day_3/decorator.py
@@ -1,12 +1,16 @@
+cnt=1
 def limit_calls(func):
+    global cnt 
+    cnt =1 
     def wrapper(*args, **kwargs):
         if(cnt < 3):
             cnt = cnt + 1
+            return func(*args, **kwargs)
         else:
             print("[BLOCKED] limit has reached buy premium")
-        return func(*args, **kwargs)
     return wrapper
 
+@limit_calls
 def send_api_request(name : str):
     print(f"Sent for {name}")
 
--- Day_3/property.py
+++ Day_3/property.py
@@ -1,23 +1,68 @@
-class Student:
-
-    def __init__(self, age):
-        self._age = age
+class Temperature:
+    def __init__(self, celsius):
+        self._celsius = celsius
+    
+    @property
+    def celsius(self):
+        print("Fetching result...")
+        return self._celsius
+    
+    @celsius.setter
+    def celsius(self, value):
+        if value < -273.15:
+            raise ValueError ("Temperature below zero")
+        print("Setting value")
+        self._celsius = value
 
     @property
-    def age(self):
-        return self._age
+    def fahernheit(self):
+        return (self._celsius * 9/5) + 32
+        
 
-    @age.setter
-    def age(self, value):
-
-        if value < 18:
-            print("Too Young")
-            return
-
-        self._age = value
+temp = Temperature(30)
+print(temp.celsius)
+temp.celsius = 30
+print(temp.fahernheit)
+temp.celsius = -300
 
 
-student = Student(20)
-print(student.age)
-student.age = 15
-print(student.age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Student:
+
+#     def __init__(self, age):
+#         self._age = age
+
+#     @property
+#     def age(self):
+#         return self._age
+
+#     @age.setter
+#     def age(self, value):
+
+#         if value < 18:
+#             print("Too Young")
+#             return
+
+#         self._age = value
+
+
+# student = Student(20)
+# print(student.age)
+# student.age = 15
+# print(student.age)
