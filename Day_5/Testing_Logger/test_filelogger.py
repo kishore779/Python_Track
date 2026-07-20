a743262aa5e2aa1578a63080ef3c0b07d7cff22c
Kishore <kishorepunniyamoorthi@gmail.com>
2026-07-20 10:48:15 +0530

    Testing File logger

---
+++ Day_5/Testing_Logger/test_filelogger.py
@@ -0,0 +1,45 @@
+import unittest
+import os
+
+from filelogger import SecureLogger, LoggerFileNotOpen
+
+class TestSecureLogger(unittest.TestCase):
+
+    def setupClass(self):
+        """
+        It Opens the files before running the each test case
+        """
+        self.filename = "file.log"
+        self.logger = SecureLogger(self.filename)
+        self.logger.open_log()
+    
+    def tearDown(self):
+
+        """
+        It closes the file after each testcase
+        """
+        self.logger.close_log()
+
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+    
+    def test_write_null_messages(self):
+
+        """
+        It tries to write null messasges in opened file
+        """
+        with self.assertRaises(TypeError):
+            self.logger.write_log(None)
+    
+    def test_write_close_logger(self):
+
+        """
+        It tries to write in closed file
+        """
+        self.logger.close_log()
+
+        with self.assertRaises(LoggerFileNotOpen):
+            self.logger.write_log("Hello this is Kishore")
+
+if __name__ == "__main__":
+    unittest.main()
--- Day_5/unittestex.py
+++ Day_5/unittestex.py
@@ -1,11 +1,36 @@
 import unittest
 
-def add(a, b):
-    return a + b
+def div(a : int, b : int) -> float:
+    try:
+        return a / b
+    except ZeroDivisionError as e:
+        return ZeroDivisionError
 
 class TestAdd(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(8,2),10)
+    """
+    Checks normal division
+    """
+    def test_numbers(self):
+        self.assertEqual(div(8,2),4)
+
+    """
+    Checks for negative outputs
+    """
+    def test_negative(self):
+        self.assertEqual(div(5,-1),-5)
+
+    """
+    Checks result for zero
+    """
+    def test_zero_case(self):
+        self.assertEqual(div(0,5), 0)
+    
+    """
+    Checks for ZeroDivion Error
+    """
+    def test_zero_div_error(self):
+        self.assertEqual(div(5,0), ZeroDivisionError)
+
 
 if __name__ == "__main__":
     unittest.main()
